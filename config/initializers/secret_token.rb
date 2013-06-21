904003e596d8c48d68061763a19eb94d8ecaa51b
mitsuhirookuno <mitsuhiro.okuno@gmail.com>
2013-06-22 01:23:17 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Sandbox::Application.config.secret_key_base = '698d657c5f99a20ce5a5134c9e23cc9019f054adfda73c92542603dee27b5f90c9cea1a1bf085f8c5b6d3fdbc64a3167e5eb9a738d0c7878ac033949f8ef28ff'
