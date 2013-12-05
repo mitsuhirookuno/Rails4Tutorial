4025bf0a688b91661b3242e8d16e7a65609d0591
mitsuhirookuno <mitsuhirookuno@gmail.com>
2013-12-05 20:14:52 +0900

    atnd_apiをとりあえずのパッチあて

--- config/initializers/add_original_format.rb
+++ config/initializers/add_original_format.rb
@@ -1,2 +1,8 @@
 Time::DATE_FORMATS[:long_jp] = '%Y年%m月%d日 %H時%M分%S秒'
 Time::DATE_FORMATS[:short_jp] = '%Y年%m月%d日'
+
+module Atndr
+  class Event
+    URL_BASE = "http://api.atnd.org/eventatnd/event/"
+  end
+end
